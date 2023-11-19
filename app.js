227c0813739107fc0d85bafd6901c31af5cf6285
Jawad <J45499738@gmail.com>
2023-11-19 06:32:41 +0500

    add all these files

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = x.length;
+//   }
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+  
+//   // Show the next 5 images
+//   for (i = slideIndex - 1; i < slideIndex + 4 && i < x.length; i++) {
+//     x[i].style.display = "block";
+//   }
+// }
+
